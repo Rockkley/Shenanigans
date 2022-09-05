6ecdf40896d94746a3619bd53c936a4f4104aa8c
Evgeniy Samarin <rockkley94@gmail.com>
2022-09-05 09:09:07 +0300

    Update Creating sublists in a list [PYTHON].py
    — Added types annotations
    — Returns instead of prints
    
    — Argument instead of input
    
    — Applying abs function to the argument and limiting its maximum to the list's size 
    
    — Some variable names are more informative now and better positioned in the code

--- Creating sublists in a list [PYTHON].py
+++ Creating sublists in a list [PYTHON].py
@@ -1,19 +1,20 @@
-a = ['Broom', 'Being', 'Boring', 'Breeding', 'Dreaming', 'Doing', 'Dancing', 'Drinking',
+words = ['Broom', 'Being', 'Boring', 'Breeding', 'Dreaming', 'Doing', 'Dancing', 'Drinking',
      'Freezing', 'Falling', 'Flooding', 'Fearing', 'Saying', 'Sleeping', 'Standing',
      'Screaming', 'Running', 'Reading', 'Rolling', 'Rushing', 'Twerking', 'Telling']
 
-def make_rows():
-    while True: # For repeating
-        row_size = int(input('Введите размер ячейки / Enter the size of a sublist '))
-        if row_size > 0:
-            # Найти кол-во групп / Calculate the amount of sublists
-            subs = (len(a) // row_size) + 1 if len(a) % row_size > 0 else len(a) // row_size
-            print(f'Слов: {str(len(a))} | | Ячеек: {str(subs)}')
-            # Создать найденное кол-во групп / Create the found amount of sublists
-            rows = [[] for i in range(subs)]; index = 0
-            for x in range(len(a)):
-                rows[index].append(a[x])
-                if len(rows[index]) == row_size: index += 1
-            print('\n' + str(rows) + '\n')
-        else: print(a)
-make_rows()
+def make_rows(row_size: int) -> list:
+    row_size = abs(int(row_size)); index = 0; amount = len(words)
+    # Найти кол-во групп / Calculate the amount of sublists
+    if row_size>amount: row_size=amount
+    if row_size > 0:
+        subs = (amount // row_size) + 1 if amount % row_size > 0 else amount // row_size
+        print(f'Слов: {len(words)} | | Ячеек: {subs}\n')
+        # Создать найденное кол-во групп / Create the found amount of sublists
+        rows = [[] for i in range(subs)]
+        for x in range(amount):
+            rows[index].append(words[x])
+            if len(rows[index]) == row_size: index += 1
+        return rows
+    else: return words
+        
+print(make_rows(2))
